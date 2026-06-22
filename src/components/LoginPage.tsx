1cca9df70cae9b108f887a73e189ef8fc8d127da
Fola <folaremidixon@gmail.com>
2026-06-22 14:36:31 +0100

    reworked on the home screen

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -259,7 +259,7 @@ export const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
                         onChange={(e) => setEmail(e.target.value)}
                         required
                         placeholder="Enter your email"
-                        className="brand-input pl-24"
+                        className="brand-input pl-[40px]"
                       />
                     </div>
                   </div>
@@ -286,7 +286,7 @@ export const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
                         onChange={(e) => setPhoneNumber(e.target.value)}
                         required
                         placeholder="123-456-7890"
-                        className="brand-input pl-24"
+                        className="brand-input pl-[40px]"
                       />
                     </div>
                   </div>
@@ -313,7 +313,7 @@ export const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
                       onChange={(e) => setPassword(e.target.value)}
                       placeholder="••••••••"
                       required
-                      className="brand-input pl-24 pr-12"
+                      className="brand-input pl-[40px] pr-12"
                     />
                     <button
                       type="button"
